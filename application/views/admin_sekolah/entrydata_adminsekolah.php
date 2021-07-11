7fef60e378a808613205e5625de06e5ae7674c32
raihanat07 <raihanaqila70@gmail.com>
2021-07-12 03:26:36 +0700

    Revert "Update entrydata_adminsekolah.php"
    This reverts commit b76e71611323662e4175aa900d91ab26c74f8281.

--- application/views/admin_sekolah/entrydata_adminsekolah.php
+++ application/views/admin_sekolah/entrydata_adminsekolah.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <!-- a -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Admin Sekolah | Entry Data</title>
